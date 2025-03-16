5ab69a52f26e02634564bb0ddb124db2364310e4
guiclipse95 <guilhermenascimento2006@gmail.com>
2025-03-16 10:36:05 -0300

    Numero de consoantes adicionada!

--- AulasDeLogica/01 - Fundamentos/repeticao.py
+++ AulasDeLogica/01 - Fundamentos/repeticao.py
@@ -1,8 +1,17 @@
 nome = input('Informe um nome: ')
 quant_vogais = 0
+quant_conso = 0
+
+# Processamento para indentificar as vogais e consoantes!
 
 for vogal in nome:
     if vogal in 'aeiouAEIOU':
         quant_vogais += 1
 
-print(f'A palavra tem {quant_vogais} vogais')
+for consoante in nome:
+    if consoante in 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ':
+        quant_conso += 1
+
+# Saida dos dados
+
+print(f'A palavra tem {quant_vogais} vogais e {quant_conso} consoantes!')
