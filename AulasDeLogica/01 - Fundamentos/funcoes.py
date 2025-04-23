b138863640fbe99e1ebe43da0f5bf3615e4d8bcc
Guilherme Nascimento <116480912+guiclipse95@users.noreply.github.com>
2025-04-23 00:54:25 -0300

    aprendizado sobre funções

---
+++ AulasDeLogica/01 - Fundamentos/funcoes.py
@@ -0,0 +1,19 @@
+
+# função: bloco de codigo "independente",
+#        executados mediante uma chamada.
+
+
+# sintaxe de declaração de uma função
+
+# def nome_funcao():
+#    print('eu sou uma função')
+# print(nome_funcao())
+
+
+
+def somar(numero1, numero2):
+    resultado = (numero1 + numero2)
+    return resultado
+
+
+print(somar(90, 10))
